ea6126abc250ceef123afa5a47c67a290e39155d
mx-smblt <mx.smblt@gmail.com>
2024-03-15 13:13:20 +0200

    Refactored the match functions.

--- test/src/main/kotlin/io/github/airflux/functional/kotest/ResultMatchers.kt
+++ test/src/main/kotlin/io/github/airflux/functional/kotest/ResultMatchers.kt
@@ -28,36 +28,20 @@ import kotlin.contracts.contract
 public inline fun <reified T, F> Result<T, F>.getValue(): T = this.shouldBeSuccess().value
 
 @OptIn(ExperimentalContracts::class)
-public inline fun <reified T> Result<T, *>.shouldBeSuccess(): Result.Success<T> {
+public inline fun <reified T, F> Result<T, F>.shouldBeSuccess(message: (F) -> String = { it.toString() }): Result.Success<T> {
     contract {
         returns() implies (this@shouldBeSuccess is Result.Success<T>)
     }
 
     if (this.isError()) {
-        errorCollector.collectOrThrow(
-            failure(
-                expected = Result.Success::class.qualifiedName!!,
-                actual = this::class.qualifiedName!!,
-                failureMessage = "Expected a `Success`, but got `${this::class.simpleName}`. "
-            )
-        )
-    }
-    return this as Result.Success<T>
-}
-
-@OptIn(ExperimentalContracts::class)
-public inline fun <reified T, F> Result<T, F>.shouldBeSuccess(message: (F) -> String): Result.Success<T> {
-    contract {
-        returns() implies (this@shouldBeSuccess is Result.Success<T>)
-    }
-
-    if (this.isError()) {
+        val expectedType = Result.Success::class.qualifiedName!!
+        val actualType = this::class.simpleName!!
         val causeDescription = message(this.cause).makeDescription()
         errorCollector.collectOrThrow(
             failure(
-                expected = Result.Success::class.qualifiedName!!,
-                actual = this::class.qualifiedName!!,
-                failureMessage = "Expected a `Success`, but got `${this::class.simpleName}`$causeDescription"
+                expected = expectedType,
+                actual = actualType,
+                failureMessage = "Expected the `Success` type, but got the `$actualType` type$causeDescription"
             )
         )
     }
@@ -77,36 +61,22 @@ public inline infix fun <reified T> Result<T, *>.shouldBeSuccess(block: (T) -> U
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun <reified E> Result<*, E>.shouldBeError(): Result.Error<E> {
+public inline fun <T, reified E> Result<T, E>.shouldBeError(
+    message: (T) -> String = { it.toString() }
+): Result.Error<E> {
     contract {
         returns() implies (this@shouldBeError is Result.Error<E>)
     }
 
     if (this.isSuccess()) {
-        errorCollector.collectOrThrow(
-            failure(
-                expected = Result.Error::class.qualifiedName!!,
-                actual = this::class.qualifiedName!!,
-                failureMessage = "Expected a `Error`, but got `${this::class.simpleName}`"
-            )
-        )
-    }
-    return this as Result.Error<E>
-}
-
-@OptIn(ExperimentalContracts::class)
-public inline fun <T, reified E> Result<T, E>.shouldBeError(message: (T) -> String): Result.Error<E> {
-    contract {
-        returns() implies (this@shouldBeError is Result.Error<E>)
-    }
-
-    if (this.isSuccess()) {
+        val expectedType = Result.Error::class.qualifiedName!!
+        val actualType = this::class.simpleName!!
         val causeDescription = message(this.value).makeDescription()
         errorCollector.collectOrThrow(
             failure(
-                expected = Result.Error::class.qualifiedName!!,
-                actual = this::class.qualifiedName!!,
-                failureMessage = "Expected a `Error`, but got `${this::class.simpleName}`$causeDescription"
+                expected = expectedType,
+                actual = actualType,
+                failureMessage = "Expected the `Error` type, but got the `$actualType` type$causeDescription"
             )
         )
     }
