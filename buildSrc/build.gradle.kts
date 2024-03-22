54f4fb6ef395cca2119214b958b5ce394506bb63
mx-smblt <mx.smblt@gmail.com>
2024-03-22 17:03:49 +0200

    Changed the version of the `pitest` plugin.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.0")
+        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
     }
 }
 
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.1")
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.2")
-    implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.0")
+    implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
     implementation("com.github.jk1:gradle-license-report:2.5")
     implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.13.2")
 }
